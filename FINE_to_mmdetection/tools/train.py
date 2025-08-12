6023c84147454c18a392e80ca74c65ed0bea7a05
LeeHyungSeop <lhhss0927@inu.ac.kr>
2025-08-12 14:47:43 +0900

    2x fpn

--- FINE_to_mmdetection/tools/train.py
+++ FINE_to_mmdetection/tools/train.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
 
 # Faster RCNN ours
 bash ./tools/dist_train.sh \
-    configs/faster_rcnn/faster-rcnn_r50_fine_fpn_1x_coco.py \
+    configs/faster_rcnn/faster-rcnn_r50_fine_fpn_2x_coco.py \
     2 \
     --cfg-options \
         train_dataloader.dataset.data_root=/media/data/coco/ \
@@ -140,8 +140,8 @@ bash ./tools/dist_train.sh \
         val_evaluator.ann_file=/media/data/coco/annotations/instances_val2017.json \
         test_evaluator.ann_file=/media/data/coco/annotations/instances_val2017.json \
         optim_wrapper.accumulative_counts=4 \
-    --work-dir ./outputs/faster-rcnn_r50_fine_fpn_1x_coco/ \
-    2>&1 | tee ./outputs/faster-rcnn_r50_fine_fpn_1x_coco/train.log
+    --work-dir ./outputs/faster-rcnn_r50_fine_fpn_2x_coco/ \
+    2>&1 | tee ./outputs/faster-rcnn_r50_fine_fpn_2x_coco/train.log
 
 
     --cfg-options optim_wrapper.accumulative_counts=4
